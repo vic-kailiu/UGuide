d7d5f63e3c4a227935b0cda56b1f69e338868ee9
Kai Liu (Victor) <vic.kai.liu@gmail.com>
2014-12-30 11:58:23 +0800

    sodu home page

--- app/src/main/java/com/kai/uguide/SelectImageActivity.java
+++ app/src/main/java/com/kai/uguide/SelectImageActivity.java
@@ -1,14 +1,26 @@
 package com.kai.uguide;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
 
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import com.kai.uguide.utils.FileUtils;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,7 +28,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class SelectImageActivity extends Activity {
+public class SelectImageActivity extends ActionBarActivity implements ObservableScrollViewCallbacks {
+
+    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
+    private static final boolean TOOLBAR_IS_STICKY = false;
+
+    private View mToolbar;
+    private View mImageView;
+    private View mOverlayView;
+    private ObservableScrollView mScrollView;
+    private TextView mTitleView;
+    private View mFab;
+    private int mActionBarSize;
+    private int mFlexibleSpaceShowFabOffset;
+    private int mFlexibleSpaceImageHeight;
+    private int mFabMargin;
+    private int mToolbarColor;
+    private boolean mFabIsShown;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_IMAGE_GALLERY = 2;
@@ -26,11 +54,53 @@ public class SelectImageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_image);
-    }
 
-    public void on_screen_click(View view) {
-        Intent i = new Intent(this, HomeActivity.class);
-        startActivity(i);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        mFlexibleSpaceShowFabOffset = getResources().getDimensionPixelSize(R.dimen.flexible_space_show_fab_offset);
+        mActionBarSize = getActionBarSize();
+        mToolbarColor = getResources().getColor(R.color.primary);
+
+        mToolbar = findViewById(R.id.toolbar);
+        if (!TOOLBAR_IS_STICKY) {
+            mToolbar.setBackgroundColor(Color.TRANSPARENT);
+        }
+        mImageView = findViewById(R.id.image);
+        mOverlayView = findViewById(R.id.overlay);
+        mScrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        mScrollView.setScrollViewCallbacks(this);
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText(getTitle());
+        setTitle(null);
+        mFab = findViewById(R.id.fab);
+        mFabMargin = getResources().getDimensionPixelSize(R.dimen.margin_standard);
+        ViewHelper.setScaleX(mFab, 0);
+        ViewHelper.setScaleY(mFab, 0);
+
+        ViewTreeObserver vto = mScrollView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    mScrollView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    mScrollView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                //mScrollView.scrollTo(0, mFlexibleSpaceImageHeight - mActionBarSize);
+
+                // If you'd like to start from scrollY == 0, don't write like this:
+                //mScrollView.scrollTo(0, 0);
+                // The initial scrollY is 0, so it won't invoke onScrollChanged().
+                // To do this, use the following:
+                onScrollChanged(0, false, false);
+
+                // You can also achieve it with the following codes.
+                // This causes scroll change from 1 to 0.
+                //mScrollView.scrollTo(0, 1);
+                //mScrollView.scrollTo(0, 0);
+            }
+        });
     }
 
     public void on_camera_click(View view) {
@@ -116,5 +186,102 @@ public class SelectImageActivity extends Activity {
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        // Translate overlay and image
+        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
+        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
+        ViewHelper.setTranslationY(mOverlayView, Math.max(minOverlayTransitionY, Math.min(0, -scrollY)));
+        ViewHelper.setTranslationY(mImageView, Math.max(minOverlayTransitionY, Math.min(0, -scrollY / 2)));
+
+        // Change alpha of overlay
+        ViewHelper.setAlpha(mOverlayView, Math.max(0, Math.min(1, (float) scrollY / flexibleRange)));
+
+        // Scale title text
+        float scale = 1 + Math.max(0, Math.min(MAX_TEXT_SCALE_DELTA, (flexibleRange - scrollY) / flexibleRange));
+        ViewHelper.setPivotX(mTitleView, 0);
+        ViewHelper.setPivotY(mTitleView, 0);
+        ViewHelper.setScaleX(mTitleView, scale);
+        ViewHelper.setScaleY(mTitleView, scale);
+
+        // Translate title text
+        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
+        int titleTranslationY = maxTitleTranslationY - scrollY;
+        if (TOOLBAR_IS_STICKY) {
+            titleTranslationY = Math.max(0, titleTranslationY);
+        }
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+
+        // Translate FAB
+        int maxFabTranslationY = mFlexibleSpaceImageHeight - mFab.getHeight() / 2;
+        int fabTranslationY = Math.max(mActionBarSize - mFab.getHeight() / 2,
+                Math.min(maxFabTranslationY, -scrollY + mFlexibleSpaceImageHeight - mFab.getHeight() / 2));
+        ViewHelper.setTranslationX(mFab, mOverlayView.getWidth() - mFabMargin - mFab.getWidth());
+        ViewHelper.setTranslationY(mFab, fabTranslationY);
+
+        // Show/hide FAB
+        if (ViewHelper.getTranslationY(mFab) < mFlexibleSpaceShowFabOffset) {
+            hideFab();
+        } else {
+            showFab();
+        }
+
+        if (TOOLBAR_IS_STICKY) {
+            // Change alpha of toolbar background
+            if (-scrollY + mFlexibleSpaceImageHeight <= mActionBarSize) {
+                setBackgroundAlpha(mToolbar, 1, mToolbarColor);
+            } else {
+                setBackgroundAlpha(mToolbar, 0, mToolbarColor);
+            }
+        } else {
+            // Translate Toolbar
+            if (scrollY < mFlexibleSpaceImageHeight) {
+                ViewHelper.setTranslationY(mToolbar, 0);
+            } else {
+                ViewHelper.setTranslationY(mToolbar, -scrollY);
+            }
+        }
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
+
+    private int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
+
+    private void showFab() {
+        if (!mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(1).scaleY(1).setDuration(200).start();
+            mFabIsShown = true;
+        }
+    }
+
+    private void hideFab() {
+        if (mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(0).scaleY(0).setDuration(200).start();
+            mFabIsShown = false;
+        }
+    }
+
+    private void setBackgroundAlpha(View view, float alpha, int baseColor) {
+        int a = Math.min(255, Math.max(0, (int) (alpha * 255))) << 24;
+        int rgb = 0x00ffffff & baseColor;
+        view.setBackgroundColor(a + rgb);
     }
 }
